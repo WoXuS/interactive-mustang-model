901cb60e093a5f162321e91a0ae30bb5f5acb450
Kamil Wozniak <kamilwozniak158@gmail.com>
2023-03-06 12:18:41 +0100

    added car on branch

---
+++ colors.js
@@ -0,0 +1,36 @@
+export const colors = [
+  {
+    texture: "./images/carbon.png",
+    size: [2, 2, 2],
+    shininess: 60,
+  },
+  {
+    color: "00000",
+  },
+  {
+    color: "FF0000",
+  },
+  {
+    color: "FFff00",
+    shininess: 60,
+  },
+  {
+    color: "c0c0c0",
+    shininess: 10,
+  },
+  {
+    color: "66533C",
+  },
+  {
+    color: "b08d56",
+  },
+  {
+    color: "153944",
+  },
+  {
+    color: "27548D",
+  },
+  {
+    color: "438AAC",
+  },
+];
